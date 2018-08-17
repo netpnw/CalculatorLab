099c4e2babfcc1ac30146985d1fa9dabdbf5a93f
600612156 <netpnw@hotmail.com>
2018-08-17 16:15:00 +0700

    Update form1

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,9 +12,129 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+
+        private float val = 0;
+        private float x;
+        
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            val = val * 10 + 4;
+            lblDisplay.Text = val.ToString();
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            val = val * 10 + 7;
+            lblDisplay.Text = val.ToString();
+
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            val = val * 10;
+            lblDisplay.Text = val.ToString();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            val = val * 10 + 1;
+            lblDisplay.Text = val.ToString();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            val = val * 10 + 2;
+            lblDisplay.Text = val.ToString();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            val = val * 10 + 3;
+            lblDisplay.Text = val.ToString();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            val = val * 10 + 5;
+            lblDisplay.Text = val.ToString();
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            val = val * 10 + 6;
+            lblDisplay.Text = val.ToString();
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            val = val * 10 + 8;
+            lblDisplay.Text = val.ToString();
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            val = val * 10 + 9;
+            lblDisplay.Text = val.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            val = val * 0;
+            lblDisplay.Text = val.ToString();
+
+        }
+        private int num = 0;
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            if (num == 0) val = x - val;
+            else if (num == 1) val = x + val;
+            else if (num == 2) val = x * val;
+            else val = x / val;
+
+
+            lblDisplay.Text = val.ToString();
+            
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            x = val;
+            val = 0;
+         
+
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            x = val;
+            val = 0;
+            num = 1;
+     
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            x = val;
+            val = 0;
+            num = 2;
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            x = val;
+            val = 0;
+            num = 3;
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }

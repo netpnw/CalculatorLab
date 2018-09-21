716a5f2dd515dd3412e6290302b70492363a9f4e
Pannawat Rodpai <netpnw@hotmail.com>
2018-09-21 18:02:02 +0700

    Can't fix process Error Test

--- CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
@@ -6,123 +6,220 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    public class CalculatorEngine
-    {
-        private bool isNumber(string str)
-        {
-            double retNum;
-            return Double.TryParse(str, out retNum);
-        }
+	public class CalculatorEngine
+	{
+		private bool isNumberPart = false;
+		protected bool isContainDot = false;
+		protected bool isSpaceAllowed = false;
 
-        private bool isOperator(string str)
-        {
-            switch(str) {
-                case "+":
-                case "-":
-                case "X":
-                case "÷":
-                    return true;
-            }
-            return false;
-        }
+		public string display = "0";
 
-        public string Process(string str)
-        {
-            string[] parts = str.Split(' ');
-            if(!(isNumber(parts[0]) && isOperator(parts[1]) && isNumber(parts[2])))
-            {
-                return "E";
-            } else
-            {
-                return calculate(parts[1], parts[0], parts[2], 4);
-            }
 
-        }
-        public string unaryCalculate(string operate, string operand, int maxOutputSize = 8)
-        {
-            switch (operate)
-            {
-                case "√":
-                    {
-                        double result;
-                        string[] parts;
-                        int remainLength;
+		public virtual string Display()
+		{
+			return display;
+		}
 
-                        result = Math.Sqrt(Convert.ToDouble(operand));
-                        // split between integer part and fractional part
-                        parts = result.ToString().Split('.');
-                        // if integer part length is already break max output, return error
-                        if (parts[0].Length > maxOutputSize)
-                        {
-                            return "E";
-                        }
-                        // calculate remaining space for fractional part.
-                        remainLength = maxOutputSize - parts[0].Length - 1;
-                        // trim the fractional part gracefully. =
-                        return result.ToString("N" + remainLength);
-                    }
-                case "1/x":
-                    if(operand != "0")
-                    {
-                        double result;
-                        string[] parts;
-                        int remainLength;
 
-                        result = (1.0 / Convert.ToDouble(operand));
-                        // split between integer part and fractional part
-                        parts = result.ToString().Split('.');
-                        // if integer part length is already break max output, return error
-                        if (parts[0].Length > maxOutputSize)
-                        {
-                            return "E";
-                        }
-                        // calculate remaining space for fractional part.
-                        remainLength = maxOutputSize - parts[0].Length - 1;
-                        // trim the fractional part gracefully. =
-                        return result.ToString("N" + remainLength);
-                    }
-                    break;
-            }
-            return "E";
-        }
+		public void handleNumber(string n)
+		{
+			if (display is "0")
+			{
+				display = "";
+			}
+			if (!isNumberPart)
+			{
+				isNumberPart = true;
+				isContainDot = false;
+			}
+			display += n;
+			isSpaceAllowed = true;
 
-        public string calculate(string operate, string firstOperand, string secondOperand, int maxOutputSize = 8)
-        {
-            switch (operate)
-            {
-                case "+":
-                    return (Convert.ToDouble(firstOperand) + Convert.ToDouble(secondOperand)).ToString();
-                case "-":
-                    return (Convert.ToDouble(firstOperand) - Convert.ToDouble(secondOperand)).ToString();
-                case "X":
-                    return (Convert.ToDouble(firstOperand) * Convert.ToDouble(secondOperand)).ToString();
-                case "÷":
-                    // Not allow devide be zero
-                    if (secondOperand != "0")
-                    {
-                        double result;
-                        string[] parts;
-                        int remainLength;
+		}
 
-                        result = (Convert.ToDouble(firstOperand) / Convert.ToDouble(secondOperand));
-                        // split between integer part and fractional part
-                        parts = result.ToString().Split('.');
-                        // if integer part length is already break max output, return error
-                        if (parts[0].Length > maxOutputSize)
-                        {
-                            return "E";
-                        }
-                        // calculate remaining space for fractional part.
-                        remainLength = maxOutputSize - parts[0].Length - 1;
-                        // trim the fractional part gracefully. =
-                        return result.ToString("N" + remainLength);
-                    }
-                    break;
-                case "%":
-                    //your code here
-                    break;
-            }
-            return "E";
-        }
-    }
+		public virtual void handleOperatorClick(string n)
+		{
+			isNumberPart = false;
+			isContainDot = false;
+			string current = display;
+
+			if (current[current.Length - 1] != ' ' || isOperator(current[current.Length - 2].ToString()))
+			{
+				display += " " + n + " ";
+				isSpaceAllowed = false;
+			}
+		}
+		public void handleSpace()
+		{
+			if (isSpaceAllowed)
+			{
+				display += " ";
+
+				isSpaceAllowed = false;
+			}
+
+		}
+
+		public void handleClear()
+		{
+			display = "0";
+			isContainDot = false;
+			isNumberPart = false;
+			isSpaceAllowed = false;
+		}
+
+		public void handleDot()
+		{
+			if (!isContainDot)
+			{
+				isContainDot = true;
+				display += ".";
+				isSpaceAllowed = false;
+			}
+		}
+
+		public void handleSign()
+		{
+			if (isNumberPart)
+			{
+				return;
+			}
+			string current = display;
+			if (current is "0")
+			{
+				display = "-";
+			}
+			else if (current[current.Length - 1] is '-')
+			{
+				display = current.Substring(0, current.Length - 1);
+				if (display is "")
+				{
+					display = "0";
+				}
+			}
+			else
+			{
+				display = current + "-";
+			}
+			isSpaceAllowed = false;
+		}
+		protected bool isNumber(string str)
+		{
+			double retNum;
+			return Double.TryParse(str, out retNum);
+		}
+
+		protected bool isOperator(string str)
+		{
+			switch (str)
+			{
+				case "+":
+				case "-":
+				case "X":
+				case "÷":
+					return true;
+			}
+			return false;
+		}
+
+		public virtual string Process(string str)
+		{
+			string[] parts = str.Split(' ');
+			if (!(isNumber(parts[0]) && isOperator(parts[1]) && isNumber(parts[2])))
+			{
+				return "E";
+			}
+			else
+			{
+				return calculate(parts[1], parts[0], parts[2], 4);
+			}
+
+		}
+		public string unaryCalculate(string operate, string operand, int maxOutputSize = 8)
+		{
+			switch (operate)
+			{
+				case "√":
+					{
+						double result;
+						string[] parts;
+						int remainLength;
+
+						result = Math.Sqrt(Convert.ToDouble(operand));
+						// split between integer part and fractional part
+						parts = result.ToString().Split('.');
+						// if integer part length is already break max output, return error
+						if (parts[0].Length > maxOutputSize)
+						{
+							return "E";
+						}
+						// calculate remaining space for fractional part.
+						remainLength = maxOutputSize - parts[0].Length - 1;
+						// trim the fractional part gracefully. =
+						return result.ToString();
+					}
+				case "1/x":
+					if (operand != "0")
+					{
+						double result;
+						string[] parts;
+						int remainLength;
+
+						result = (1.0 / Convert.ToDouble(operand));
+						// split between integer part and fractional part
+						parts = result.ToString().Split('.');
+						// if integer part length is already break max output, return error
+						if (parts[0].Length > maxOutputSize)
+						{
+							return "E";
+						}
+						// calculate remaining space for fractional part.
+						remainLength = maxOutputSize - parts[0].Length - 1;
+						// trim the fractional part gracefully. =
+						return result.ToString();
+					}
+					break;
+			}
+			return "E";
+		}
+
+		public string calculate(string operate, string firstOperand, string secondOperand, int maxOutputSize = 8)
+		{
+			switch (operate)
+			{
+				case "+":
+					return (Convert.ToDouble(firstOperand) + Convert.ToDouble(secondOperand)).ToString();
+				case "-":
+					return (Convert.ToDouble(firstOperand) - Convert.ToDouble(secondOperand)).ToString();
+				case "X":
+					return (Convert.ToDouble(firstOperand) * Convert.ToDouble(secondOperand)).ToString();
+				case "÷":
+					// Not allow devide be zero
+					if (secondOperand != "0")
+					{
+						double result;
+						string[] parts;
+						int remainLength;
+
+						result = (Convert.ToDouble(firstOperand) / Convert.ToDouble(secondOperand));
+						// split between integer part and fractional part
+						parts = result.ToString().Split('.');
+						// if integer part length is already break max output, return error
+						if (parts[0].Length > maxOutputSize)
+						{
+							return "E";
+						}
+						// calculate remaining space for fractional part.
+						remainLength = maxOutputSize - parts[0].Length - 1;
+						// trim the fractional part gracefully. =
+						return result.ToString();
+					}
+					break;
+				case "%":
+					//your code here
+					break;
+			}
+			return "E";
+		}
+	}
 }
--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -6,12 +6,41 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    public class RPNCalculatorEngine
+    public class RPNCalculatorEngine : CalculatorEngine
     {
-        public string Process(string str)
+        public override string Process(string str)
         {
-            // your code here
-            return "E";
+			// your code here
+			string first, second, third, four;
+			Stack<string> rpn = new Stack<string>();
+
+			string[] parts = str.Split(' ');
+			
+			for (int i = 0; i < parts.Length; i++)
+			{
+				if (isNumber(parts[i]))
+				{
+					rpn.Push(parts[i]);
+				}
+
+				if (isOperator(parts[i]))
+				{
+					second = rpn.Pop();
+					first = rpn.Pop();
+					four = calculate(parts[i], first, second, 4);
+					rpn.Push(four);
+				}
+			}
+			
+				if (!(isNumber(parts[0]) && isOperator(parts[1]) && isNumber(parts[2])))
+				{
+					return rpn.Peek();
+				}
+				else
+				{
+					return "E";
+				}
+			
         }
     }
 }
